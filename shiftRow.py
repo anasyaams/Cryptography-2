f71c8c36f92ccccad4b36230381390a5e1e4144b
Anasya Miftakus <100329265+anasyaams@users.noreply.github.com>
2023-01-18 08:16:03 +0700

    Create shiftRow.py

---
+++ shiftRow.py
@@ -0,0 +1,17 @@
+#Shift Row in AES Encription
+def ShiftRow(msg):
+  state = [[] for j in range(4)]
+  for r in range(4):
+    for c in range(4):
+      state[r].append(msg[r + 4 * c])
+  
+  state = np.array(state)
+
+  for i in range(1, 4):
+      state[i] = np.append(state[i, i:], state[i, :i])
+  
+  return state
+
+#test
+pt = "FUNGSISHIFTROW80"
+print(ShiftRow(pt))
